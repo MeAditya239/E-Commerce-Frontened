35478f1ff5b5c015ea0bbce04a344ef5d93af6e9
MeAditya239 <adityaspatil239@gmail.com>
2025-09-07 15:43:09 +0530

    done it

--- src/customer/components/Checkout/OrderSummary.jsx
+++ src/customer/components/Checkout/OrderSummary.jsx
@@ -5,34 +5,38 @@ import { Button } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
 import { getOrderById } from "../../../state/Order/Action";
 import { useLocation } from "react-router-dom";
+import { createPayment } from "../../../state/Payment/Action";
 
 const OrderSummary = () => {
-
   const dispatch = useDispatch();
   const location = useLocation();
-  const { order } = useSelector( store => store);
+  const { order } = useSelector((store) => store);
 
-  const searchParams = new URLSearchParams( location.search);
+  const searchParams = new URLSearchParams(location.search);
   const orderId = searchParams.get("order_id");
 
   useEffect(() => {
-    dispatch(getOrderById( orderId));
-  }, [orderId])
+    dispatch(getOrderById(orderId));
+  }, [orderId]);
+
+
+  const handleCheckout  =() => {
+    dispatch( createPayment(orderId));
+  }
 
   return (
     <div>
       <div className="p-5 shadow-lg rounded-s-md border">
-        { console.log("order shiping address - ", order.order?.shipingAddress) }
-        <AddressCard address ={ order.order?.shipingAddress } />
+        {console.log("order shiping address - ", order.order?.shipingAddress)}
+        <AddressCard address={order.order?.shipingAddress} />
       </div>
 
       <div>
         <div className="lg:grid grid-cols-3  relative">
           <div className="col-span-2">
             {order.order?.orderItem.map((item) => (
-              <CartItem item ={ item} />
+              <CartItem item={item} />
             ))}
-            
           </div>
 
           <div className="px-5 sticky top-0 h-[100vh] mt-5 lg:mt-0 ">
@@ -60,7 +64,9 @@ const OrderSummary = () => {
 
                 <div className="flex justify-between pt-3 font-bold">
                   <span>Total Amount</span>
-                  <span className=" text-green-600">₹{order.order?.totalDiscountedPrice}</span>
+                  <span className=" text-green-600">
+                    ₹{order.order?.totalDiscountedPrice}
+                  </span>
                 </div>
               </div>
 
@@ -69,6 +75,7 @@ const OrderSummary = () => {
                 color="error"
                 size="medium"
                 className="mt-3 w-full py-2.5 px-5 rounded-md bg-indigo-600 text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                onClick={handleCheckout}
               >
                 Checkout
               </Button>
